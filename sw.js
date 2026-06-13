5c3f7f467096a27bf3e2ae14ae1ec7a6664ab552
sarimrehan003-hash <sarimrehan003@gmail.com>
2026-06-13 13:41:04 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fwps-v30';
+const CACHE = 'fwps-v31';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
